9c547cd5f5055f26ac641b03cc0a59f87ddd9569
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-07-01 20:45:37 -0400

    two

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const PORT = process.env.PORT || 3001;
 
 const express = require('express');
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
@@ -25,6 +29,14 @@ app.get('/api/animals/:id', (req, res) => {
 function findById(id, animalsArray) {
   const result = animalsArray.filter((animal) => animal.id === id)[0];
   return result;
+}
+function createNewAnimal(body, animalsArray) {
+  console.log(body);
+  // our function's main code will go here!
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  res.json(req.body);
 }
 app.post('/api/animals', (req, res) => {
   console.log(req.body);
